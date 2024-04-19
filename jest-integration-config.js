bafc68e667757861b93dec060d3f01416936f6e0
mykaellyoliveira <mykaelly.odc02@gmail.com>
2024-04-19 16:16:35 -0300

    chore: adjustment imports jest config

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,3 +1,4 @@
-import config from './jest.config'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require('./jest.config')
 config.testMatch = ['**/*.test.ts']
 module.exports = config
