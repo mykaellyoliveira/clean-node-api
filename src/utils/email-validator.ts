e598fd74434a89cd5f178a13f8629aa18021d1e7
mykaellyoliveira <mykaelly.odc02@gmail.com>
2024-04-19 16:40:17 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,8 +1,9 @@
 import { type EmailValidator } from '../presentation/protocols/email-validator'
+import validator from 'validator'
 // esse componente EmailValidatorAdapter será utilizado para encapsular o componente do validador de email
 // Criando essa classe estamos isolando dos controladores a validação de e-mail, ou seja é possível utilizar separada
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
-    return false
+    return validator.isEmail(email)
   }
 }
